96af1fd83be5ce7b670ad4d214c001e17cd37ede
andreamazza84 <andrea.mazza84@gmail.com>
2021-01-28 17:59:05 +0100

    Consegna esercizio

--- index.php
+++ index.php
@@ -5,11 +5,9 @@ $text = "L'assioma di Murphy, in senso stretto e in questa formulazione presunta
 Tuttavia il senso che assumono le Leggi di Murphy successivamente diffuse, del tutto ironico e paradossale, è diverso: in queste si afferma infatti che l'evento considerato inizialmente improbabile, alla prova dei fatti si verifica spesso o addirittura sempre, ovverosia al primo tentativo o esperimento. In questo si riassume l'effetto ironico delle frasi, ove quasi sempre l'evento in oggetto è considerato inizialmente improbabile proprio perché è un evento spiacevole e sgradito dal quale si tenta di proteggersi, ma che nei fatti si verifica nonostante la opposta valutazione iniziale che era sbagliata ma apparentemente non tale (per ragioni di soggettività della valutazione).
 Pertanto queste leggi sono affermazioni caricaturali sulla realtà, che distorcono tanto la reale frequenza o probabilità di un fatto (dichiarandolo più probabile di quanto non sia veramente), tanto le valutazioni preventive operabili sul fatto stesso (confondendo la sgradevolezza e l'indesiderabilità di un fatto con la sua probabilità, che viene presunta essere alta in modo del tutto ingiustificato). ";
 
-// $badword = $argv[1];
-$badword = 'Murphy';
+$badword = $_GET["badword"];
 
-$position = strpos($text, $badword);
-var_dump($position);
+$newText = str_replace($badword, '<strong>***</strong>', $text);
 
 ?>
 
@@ -19,13 +17,22 @@ var_dump($position);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-badwords</title>
+    <style>strong{color: red;}</style>
 </head>
 <body style="background: #f7f7f7; font-family: Arial;">
-<div class="container" style="width: 80%; margin: auto;">
-    <h1>La legge di Murphy</h1>
-    <p style="line-height: 1.5; text-align: justify;"><?php echo $text;?></p>
-    <h3><?php echo $badword;?></h3>
-</div>
+    <div class="container" style="width: 80%; margin: auto;">
+        <h1>La legge di Murphy</h1>
+        <p style="line-height: 1.5; text-align: justify;"><?php echo $text;?></p>
+        
+        <form action="index.php">
+            <input name="badword" type="text">
+            <input value="Send badword" type="submit">
+        </form>
+        
+        <h3>La badword "<?php echo $badword;?>" viene sostituita con: <strong>***</strong></h3>
+        <p style="line-height: 1.5; text-align: justify;"><?php echo $newText;?></p>
+
+    </div>
 </body>
 </html>
 
